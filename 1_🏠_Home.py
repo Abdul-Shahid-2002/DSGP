31585ff2cd6c56711a6576be48bb362387fba649
Abdul-Shahid-2002 <shahidafham7773@gmail.com>
2024-03-19 15:58:01 +0530

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -30,10 +30,17 @@ if authentication_status:
 
     st.header('🌊Test the Waters')
     st.subheader('Understand your customers and competition before you make decisions')
+    st.write("Research")
+    st.write("Structured: Chat with tabular data store in a SQL database")
+    st.write("            Data contains Customers and Watches    ")
+    st.write("Unstructured: Chat with textual data stored in a Vector Database")
+    st.write("              Contains textual data about some watches")
 
     st.header('🌊Take the plunge')
     st.subheader('Generate tailor made marketing decisions unique to each customer')
-
+    st.write("Research analyzer: A Chat bot used to analyze customer sentiments on Watch types")
+    st.write("Decisions: Analyse Customer plots . ")
+    st.write("           Analyze Customer segments using kmeans and Vector database")
     authenticator.logout("Logout", "sidebar")
 
 
